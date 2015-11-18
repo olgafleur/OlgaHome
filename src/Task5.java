cb1996b4930efb7f78ccedd263a4d559912ff34e
olgafleur <o.goryshnyak@gmail.com>
2015-11-18 14:16:31 +0200

    Task5

---
+++ src/Task5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+
+        if (x > y )
+        {
+            System.out.println(x - y);
+        }
+
+        if (y > x)
+        {
+            System.out.println(x+=y);
+        }
+
+        else {
+            System.out.println(" ");
+        }
+
+    }
+}
+ 
