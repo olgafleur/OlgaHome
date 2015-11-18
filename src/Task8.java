5393b79022d483c33f2bf498359a89194af449b0
olgafleur <o.goryshnyak@gmail.com>
2015-11-18 14:17:40 +0200

    Task8

---
+++ src/Task8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Task8 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        if (x%10 == y)
+        {
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+
+
+    }
+}
