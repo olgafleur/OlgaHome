cd3c257bb86cbf9393bcc2c265f9fa9af4e4f367
olgafleur <o.goryshnyak@gmail.com>
2015-11-18 14:15:20 +0200

    Task4

---
+++ src/Task4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Task4 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        double x = sc.nextDouble();
+
+        if (x >= 0 && x <= 1) {
+
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+
+        }
+    }
+}
+ 
